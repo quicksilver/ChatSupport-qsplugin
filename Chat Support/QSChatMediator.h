b4d3db2c0f6a3327ab93056b25b9072627bef6e6
Rob McBroom <github@skurfer.com>
2012-10-19 09:28:21 -0400

    Initial commit for the new Chat Support plug-in

---
+++ Chat Support/QSChatMediator.h
@@ -0,0 +1,10 @@
+//
+//  QSChatMediator.h
+//  Chat Support
+//
+//  Created by Rob McBroom on 2012/10/18.
+//
+
+@interface QSChatMediator : NSObject
+
+@end
---
+++ Chat Support/QSChatMediator.m
@@ -0,0 +1,12 @@
+//
+//  QSChatMediator.m
+//  Chat Support
+//
+//  Created by Rob McBroom on 2012/10/18.
+//
+
+#import "QSChatMediator.h"
+
+@implementation QSChatMediator
+
+@end
